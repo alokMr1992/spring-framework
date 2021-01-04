efbfccb6a42fa17f932fe06fb99336385fdc6aa9
Alok Mrityunjay <alok.mrityunjay1992@gmail.com>
2021-01-05 00:22:14 +0530

    Proxy for Prototype dependency under Singleton bean

--- src/main/java/com/spring/basics/scope/PersonDao.java
+++ src/main/java/com/spring/basics/scope/PersonDao.java
@@ -1,12 +1,9 @@
 package com.spring.basics.scope;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class PersonDao {
 	
 	@Autowired
