561a64847df9e537d38efc617314d78d6ef08929
Alok Mrityunjay <alok.mrityunjay1992@gmail.com>
2021-01-08 17:09:21 +0530

    Converting spring boot into spring application

--- src/main/java/com/spring/basics/SpringFrameworkBasicApplication.java
+++ src/main/java/com/spring/basics/SpringFrameworkBasicApplication.java
@@ -1,6 +1,5 @@
 package com.spring.basics;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -8,21 +7,21 @@ import org.springframework.context.annotation.Configuration;
 import com.spring.basics.basic.BinarySearchImpl;
 
 @Configuration
-@ComponentScan
+@ComponentScan("com.spring.basics.basic")
 public class SpringFrameworkBasicApplication {
 
 	public static void main(String[] args) {
 		
-		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringFrameworkBasicApplication.class);
-		
-		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
-		
-		BinarySearchImpl binarySearch2 = applicationContext.getBean(BinarySearchImpl.class);
-		
-		System.out.println(binarySearch);
-		System.out.println(binarySearch2);
-		
-		int result = binarySearch.binarySearch(new int[] {1, 2, 12, 7}, 2);
-		System.out.println("Result = " + result);
+		try(AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringFrameworkBasicApplication.class)) {
+			BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+			
+			BinarySearchImpl binarySearch2 = applicationContext.getBean(BinarySearchImpl.class);
+			
+			System.out.println(binarySearch);
+			System.out.println(binarySearch2);
+			
+			int result = binarySearch.binarySearch(new int[] {1, 2, 12, 7}, 2);
+			System.out.println("Result = " + result);
+		}
 	}
 }
--- src/main/java/com/spring/basics/SpringFrameworkComponentScanApplication.java
+++ src/main/java/com/spring/basics/SpringFrameworkComponentScanApplication.java
@@ -2,14 +2,13 @@ package com.spring.basics;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import com.spring.component.ComponentDao;
 
-@SpringBootApplication
+@Configuration
 @ComponentScan("com.spring.component")
 public class SpringFrameworkComponentScanApplication {
 	
@@ -17,15 +16,15 @@ public class SpringFrameworkComponentScanApplication {
 
 	public static void main(String[] args) {
 		
-		ApplicationContext applicationContext = SpringApplication.run(SpringFrameworkComponentScanApplication.class, args);
-		
-		ComponentDao componentDao = applicationContext.getBean(ComponentDao.class);
-		ComponentDao componentDao2 = applicationContext.getBean(ComponentDao.class);
-		
-		LOGGER.info("{}", componentDao);
-		LOGGER.info("{}", componentDao.getComponentJdbcConnection());
-		
-		LOGGER.info("{}", componentDao2);
-		LOGGER.info("{}", componentDao2.getComponentJdbcConnection());
+		try(AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringFrameworkComponentScanApplication.class)){
+			ComponentDao componentDao = applicationContext.getBean(ComponentDao.class);
+			ComponentDao componentDao2 = applicationContext.getBean(ComponentDao.class);
+			
+			LOGGER.info("{}", componentDao);
+			LOGGER.info("{}", componentDao.getComponentJdbcConnection());
+			
+			LOGGER.info("{}", componentDao2);
+			LOGGER.info("{}", componentDao2.getComponentJdbcConnection());
+		}
 	}
 }
