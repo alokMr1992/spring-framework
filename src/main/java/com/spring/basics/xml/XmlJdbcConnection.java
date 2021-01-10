65b7373b0a4682881735961f749459420e53e55b
Alok Mrityunjay <alok.mrityunjay1992@gmail.com>
2021-01-10 23:57:32 +0530

    Creating POJOs for XML based Application Context

---
+++ src/main/java/com/spring/basics/xml/XmlJdbcConnection.java
@@ -0,0 +1,8 @@
+package com.spring.basics.xml;
+
+public class XmlJdbcConnection {
+	
+	public XmlJdbcConnection() {
+		System.out.println("XML Jdbc Connection");
+	}
+}
---
+++ src/main/java/com/spring/basics/xml/XmlPersonDao.java
@@ -0,0 +1,14 @@
+package com.spring.basics.xml;
+
+public class XmlPersonDao {
+	
+	private XmlJdbcConnection xmlJdbcConnection;
+
+	public XmlJdbcConnection getXmlJdbcConnection() {
+		return xmlJdbcConnection;
+	}
+
+	public void setXmlJdbcConnection(XmlJdbcConnection xmlJdbcConnection) {
+		this.xmlJdbcConnection = xmlJdbcConnection;
+	}
+}
