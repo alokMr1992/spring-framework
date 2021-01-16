1073091b034730e558c88bddc491ae65ccf3564d
Alok Mrityunjay <alok.mrityunjay1992@gmail.com>
2021-01-16 15:45:54 +0530

    Use of @Bean on a method - will be invoked on the server startup

--- src/main/java/com/spring/basics/basic/BinarySearchImpl.java
+++ src/main/java/com/spring/basics/basic/BinarySearchImpl.java
@@ -3,6 +3,7 @@ package com.spring.basics.basic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -19,5 +20,10 @@ public class BinarySearchImpl {
 		System.out.println(sortAlgo);
 		
 		return 3;
+	}
+	
+	@Bean
+	public void doSomething() {
+		System.out.println("Do something under BinarySearchImpl");
 	}
 }
